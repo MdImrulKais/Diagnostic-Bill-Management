3721c8ab4b4a4aefec5e93d4a1d04f77554e158e
MdImrulKais <30573851+MdImrulKais@users.noreply.github.com>
2018-10-11 22:14:00 +0600

    Add files via upload

---
+++ CommonGateway.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace DiagnosticBillManagementSystem.DAL
+{
+    public class CommonGateway
+    {
+        protected string connectionSrting = WebConfigurationManager.ConnectionStrings["DiagnosticBillManagementConnectionString"].ConnectionString;
+        public string Query { get; set; }
+        public SqlConnection Connection { get; set; }
+        public SqlCommand Command { get; set; }
+        public SqlDataReader Reader { get; set; }
+
+        public CommonGateway()
+        {
+            Connection = new SqlConnection(connectionSrting);
+        }
+    }
+}
