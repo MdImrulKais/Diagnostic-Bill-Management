8d1a74ec350f6b89f5c1ac68c5de3052858143ad
MdImrulKais <30573851+MdImrulKais@users.noreply.github.com>
2018-10-11 22:16:35 +0600

    Add files via upload

---
+++ Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiagnosticBillManagementSystem.Models.EntityModels
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string Name { get; set; }
+        public string DateOfBirth { get; set; }
+        public string MobileNo { get; set; }
+        public double BillAmount { get; set; }
+        public int PaymentStatus { get; set; }
+    }
+}
