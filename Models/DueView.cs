7fe38182e38eb6b8e5a8b158a24d78f02b72eba8
MdImrulKais <30573851+MdImrulKais@users.noreply.github.com>
2018-10-11 22:19:44 +0600

    Add files via upload

---
+++ Models/DueView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiagnosticBillManagementSystem.Models.ViewModels
+{
+    public class DueView
+    {
+        public int PatientId { get; set; }
+        public string Amount { get; set; }
+        public string DueDate { get; set; }
+    }
+}
